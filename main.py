faf5b67bf86a5ebeb2d0700b535372366057b3af
Ken Sible <ksible@outlook.com>
2023-02-13 22:41:38 -0500

    added phonetic transcription (IPA)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import requests, fitz, json, os, re
+import requests, urllib, random, fitz, json, tqdm, os, re
 
 WORD_LIST_URL = 'https://www.goethe.de/pro/relaunch/prf/en/Goethe-Zertifikat_B1_Wortliste.pdf'
 FREQ_DICT_URL = 'https://en.wiktionary.org/wiki/Wiktionary:Frequency_lists/A_Frequency_Dictionary_of_German'
@@ -22,11 +22,22 @@ def extract_word_list(filename):
                     word_list[blocks[i]] = blocks[i + N]
     return word_list
 
+def get_pronunciation(word):
+    for w in (word, word.capitalize()):
+        try:
+            page = requests.get(f'https://de.wiktionary.org/wiki/{w}')
+            soup = BeautifulSoup(page.text, 'html.parser')
+            text = soup.find_all('span', {'class': 'ipa'})
+            return text[0].getText()
+        except (urllib.error.HTTPError, IndexError): pass
+    return ''
+
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-w', action='store_true', help='construct word_list')
     parser.add_argument('-f', action='store_true', help='construct freq_list')
+    parser.add_argument('-p', action='store_true', help='include pronunciation')
     args = parser.parse_args()
 
     if args.w:
@@ -39,21 +50,29 @@ if __name__ == '__main__':
         page = requests.get(FREQ_DICT_URL)
         soup = BeautifulSoup(page.content, 'html.parser')
         text = soup.find_all('ol')[0].get_text().split('\n')
-        freq = {word: i for i, word in enumerate(text, start=1)}
+        freq_list = {word: i for i, word in enumerate(text, start=1)}
         with open('freq_list.json', 'w') as outfile:
-            json.dump(freq, outfile)
+            json.dump(freq_list, outfile)
 
     with open('word_list.json') as word_file, open('freq_list.json') as freq_file:
         word_list = json.load(word_file)
         freq_list = json.load(freq_file)
-        filter = r'(der|die|das)\s|\(sich\)\s|sich\s|\-$|\s\(Pl.\)'
+        pattern = r'(der|die|das)\s|\(sich\)\s|sich\s|\-$|\s\(Pl.\)'
 
         word_freq = []
-        count = total = 0
-        for word in word_list:
-            word = re.sub(filter, '', re.split(r'[,/]', word)[0])
-            if word not in freq:
-                print(word)
-            else: count += 1
-            total += 1
-        print('\n%0.2f %%' % (count / total * 100))
+        remainder = []
+        for entry in word_list:
+            word = re.sub(pattern, '', re.split(r'[,/]', entry)[0])
+            if word in freq_list:
+                word_freq.append((entry, word, freq_list[word]))
+            else:
+                remainder.append((entry, word, None))
+        random.shuffle(remainder)
+        print('Frequency Coverage: %0.2f %%' % (len(word_freq) / len(word_list) * 100))
+
+    word_freq.sort(key=lambda x: x[1])
+    word_freq.extend(remainder)
+    with open('word_list.txt', 'w') as outfile:
+        for i, (entry, word, _) in tqdm.tqdm(list(enumerate(word_freq))):
+            pronunciation = get_pronunciation(word) if args.p else ''
+            outfile.write(f'{i} [TODO] ;; {entry} ;; {pronunciation} ;; {word_list[entry]}\n')
